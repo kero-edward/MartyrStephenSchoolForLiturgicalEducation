9d2a46f00198d5832633837995d4df6e60215b3c
kero-edward <kyrillos.edward17@gmail.com>
2023-12-13 02:29:03 +0200

    details component finished

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import copticData from '../../json/data.json';
 import { BehaviorSubject } from 'rxjs';
@@ -13,14 +13,22 @@ export class DetailsComponent implements OnInit {
   letter?: any = {};
   getData: any[] = copticData;
   play: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  screenWidth: any;
 
   audioEnded(): void {
     this.play.next(false);
   }
 
   constructor(private route: ActivatedRoute) {
+    this.getScreenSize();
     this.letterId = this.route.snapshot.params['id'];
     this.letter = this.getData.find((item) => item.id == this.letterId);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    this.screenWidth = window.innerWidth;
+    console.log('screenWidth: ', this.screenWidth);
   }
 
   ngOnInit(): void {}
